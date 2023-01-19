bb313a4c4212ea3b0618754e9c4561f30cb1f149
radoslavPetrani <radoslavpetrani@gmail.com>
2023-01-19 13:47:06 +0100

    Structure of Books + Jsons done

---
+++ flutter_library/lib/chapter_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_library/Objects/sections.dart';
+
+class ChapterView extends StatelessWidget {
+  const ChapterView({super.key, required this.title, required this.sections});
+  final String title;
+  final List<Section> sections;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: _buildSectionsPageView(),
+    );
+  }
+
+  Widget _buildSectionsPageView() {
+    return PageView.builder(
+        itemCount: sections.length,
+        itemBuilder: (context, index) {
+          return Container(
+            child: Center(
+              child: Text(sections.elementAt(index).content),
+            ),
+          );
+        });
+  }
+}
